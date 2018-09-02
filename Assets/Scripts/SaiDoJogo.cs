5f4b2fcf9c3b2ea1085599106d2d9448672c38f6
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-09-02 17:32:15 -0300

    Victory

---
+++ Assets/Scripts/SaiDoJogo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaiDoJogo : MonoBehaviour {
+
+    public GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+        gm.Victory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
