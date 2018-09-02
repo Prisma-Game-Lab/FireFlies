1059ba2c468d4a1ec15923329decec78602473ca
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-09-02 00:14:59 -0300

    Checkpoint

---
+++ ProjectGlubGlub/Assets/Scripts/CheckPointTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointTrigger : MonoBehaviour {
+
+    private RespawnPlayer respawnPlayerComponent;
+    private GameObject lava;
+
+	private void OnEnable()
+	{
+        lava = GameObject.Find("Lava");
+        respawnPlayerComponent = lava.GetComponent<RespawnPlayer>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if(collision.tag == "Player"){
+            respawnPlayerComponent.RespawnPosition = this.transform.position;
+            this.gameObject.SetActive(false);
+        }
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		
+	}
+}
