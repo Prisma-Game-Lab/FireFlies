451876c80ba377512b43694e62b52ac08cfd3388
Michelle Beadle <michellesantiagobeadle@hotmail.com>
2018-09-01 22:59:24 -0300

    Surprise mothafucker

--- ProjectGlubGlub/Assets/Scripts/LavaRising.cs
+++ ProjectGlubGlub/Assets/Scripts/LavaRising.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class LavaRising : MonoBehaviour {
 
+    public float risePercent = 0.00001f;
+    public float secondsforRise = 1.0f; 
+
+
 	// Use this for initialization
 	void Start () {
-		
+        StartCoroutine(riseLava());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    IEnumerator riseLava(){
+
+        while(true){
+            yield return new WaitForSeconds(secondsforRise);
+            this.transform.position += new Vector3(0,risePercent,0);
+        }
+    }
 }
