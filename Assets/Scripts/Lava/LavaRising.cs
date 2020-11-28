9e235e05df2d6569ee6e5b9d61d260d1d1b972dc
Michelle Santiago <michelle.santiago10@gmail.com>
2020-11-28 16:27:17 -0300

    Comentários no script de lava

---
+++ Assets/Scripts/Lava/LavaRising.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaRising : MonoBehaviour {
+
+    // É a variavel que espera 1 segundo quando o player respawna
+    public float initialDelay = 1.0f;
+    public float risePercent = 0.1f;
+    //public float secondsforRise = 1.0f;
+
+    [HideInInspector]
+    // Assim que foi respawnado
+    public bool justRespawned = true;
+
+    private float currentInitialDelay = 0;
+    private float currentSecondsForRise = 0;
+
+    // Espera um tempo inicial antes de começara subir a lava
+    private bool isRising = false;
+
+	// Use this for initialization
+	void Start () {
+        //StartLavaRising();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        // Ele respawnou e ajeitou a variável isRising
+        if (!justRespawned) {
+
+            // Se a lava não está subindo, é porque ele tá esperando você respawnar para subir
+            if(!isRising){
+
+                // Quando o player acaba de respawnar, espera um tempo antes de fazer a lava subir
+                currentInitialDelay += Time.deltaTime;
+
+                // Quando passa um segundo ele faz a lava subir
+                if(currentInitialDelay >= initialDelay){
+                    isRising = true;
+                }
+
+            // Esperou o tempo que precisava para o player respawnar
+            } else {
+
+                // Sobe a lava a cada frame em cima da porcentágem indicada
+                this.transform.position += new Vector3(0, risePercent, 0); 
+            }
+         
+        // Ele acabou de respawnar
+        } else {
+
+            // Para de movimentar a lava 
+            isRising = false;
+            justRespawned = false;
+
+            // Reinicia o timer para esperar para que a lava suba
+            currentInitialDelay = 0;
+        }
+
+    }
+
+    public void StartLavaRising(){
+        //StartCoroutine(waitStart());
+    }
+
+    /*IEnumerator waitStart(){
+
+        yield return new WaitForSeconds(initialDelay);
+        isNotRespawn = true;
+        StartCoroutine(riseLava());
+    }
+
+    IEnumerator riseLava(){
+        
+        while(isNotRespawn){
+            yield return new WaitForSeconds(secondsforRise);
+            this.transform.position += new Vector3(0,risePercent,0);
+        }
+    }*/
+}
---
+++ Assets/Scripts/Lava/RespawnPlayer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour {
+
+    [HideInInspector]
+    public Vector3 RespawnPosition; // VERIFICAR ONDE ELE ATUALIZA ESSE VALOR
+    public float lavaRespawn = 35;
+
+	private void OnEnable()
+	{
+        RespawnPosition = GameObject.Find("Player").transform.position;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        RespawnaPlayer(collision.gameObject.transform);
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		
+	}
+
+
+    public void RespawnaPlayer(Transform go){
+
+        if(go.tag == "Player"){
+
+            // Volta o player pra respawn position (que é atualizada a cada checkpoint que ele pega)
+            go.transform.position = RespawnPosition;
+
+            // Seta a velocidade do player como zero após a troca de posição
+            go.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+
+            GameObject.Find("Camera").GetComponent<FollowPlayer>().updateCenterCam(RespawnPosition.y + 5);
+
+            //Acabou de respawnar o player no checkpoint
+            GameObject.Find("Lava").GetComponent<LavaRising>().justRespawned = true;
+
+            // coloca a lava abaixo do respawn do player
+            GameObject.Find("Lava").transform.position = new Vector3(GameObject.Find("Lava").transform.position.x, RespawnPosition.y - lavaRespawn, 0);
+            //GameObject.Find("Lava").GetComponent<LavaRising>().StartLavaRising();
+        }
+    }
+}
