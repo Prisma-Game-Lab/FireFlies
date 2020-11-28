3d43879792aaa9f07a716b1c14ee9d689d770cd2
Lucas Lima <lucaslimareis3311@gmail.com>
2020-11-28 15:23:43 -0300

    webgl template + cena de menu fixed

---
+++ Assets/Scripts/loadSceneOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadSceneOnCollision : MonoBehaviour {
+
+    public GameObject gameManager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            gameManager.GetComponent<GameManager>().ChangeToGame();
+        }
+    }
+}
