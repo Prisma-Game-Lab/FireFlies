203840abad44c15a9d7a2656e1fbf84d1c19d2d9
Lucas Lima <lucaslimareis3311@gmail.com>
2020-11-28 19:52:25 -0300

    fix indo pra menu scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour {
     }
 
     public void BackToMenu(){
-        SceneManager.LoadScene("StartScene");
+        SceneManager.LoadScene("MenuScene");
     }
 
     public void Victory()
